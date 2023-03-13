1b63a1ba803f3fb38ebe964ad5c83ba8076d3686
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-13 20:01:07 +0100

    SW03: add Buchklasse

---
+++ Uebungen/SW03/Library/Library.Host/Buch.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Host;
+
+public class Buch
+{
+    private readonly string titel;
+    private readonly string autor;
+    private readonly DateTime veroeffentlichungsdatum;
+
+    public Buch(string titel, string autor, DateTime veroeffentlichungsdatum)
+    {
+        this.titel = titel;
+        this.autor = autor;
+        this.veroeffentlichungsdatum = veroeffentlichungsdatum;
+    }
+}
