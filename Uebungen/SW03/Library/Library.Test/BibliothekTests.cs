bce89aacc84475b9b9f8aba1bc05486e877ad72a
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-13 20:05:35 +0100

    SW03: Fuege Bibliothek und Tests hinzu

---
+++ Uebungen/SW03/Library/Library.Test/BibliothekTests.cs
@@ -0,0 +1,13 @@
+using Library.Host;
+
+namespace Library.Test;
+
+public class BibliothekTests
+{
+    private readonly Bibliothek testee;
+
+    public BibliothekTests()
+    {
+        this.testee = new Bibliothek();
+    }
+}
