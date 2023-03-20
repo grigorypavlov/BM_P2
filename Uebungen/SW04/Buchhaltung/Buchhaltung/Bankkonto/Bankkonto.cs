1adfd3a27ee7966475d9878a1e6fcbaa3fd52ee7
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-20 07:34:54 +0100

    add solution

---
+++ Uebungen/SW04/Buchhaltung/Buchhaltung/Bankkonto/Bankkonto.cs
@@ -0,0 +1,55 @@
+﻿namespace Buchhaltung;
+
+public class Bankkonto
+{
+    private string kontonummer;
+    private decimal kontostand;
+    private List<Transaktion> transaktionen;
+
+    public Bankkonto(string kontonummer, decimal anfangsKontostand)
+    {
+        this.kontonummer = kontonummer;
+        this.kontostand = anfangsKontostand;
+        this.transaktionen = new List<Transaktion>();
+    }
+
+    public decimal Kontostand
+    {
+        get { return kontostand; }
+        private set { kontostand = value; }
+    }
+
+    public void Einzahlen(decimal betrag)
+    {
+        if (betrag <= 0)
+        {
+            throw new ArgumentException("Der Einzahlungsbetrag muss größer als Null sein.");
+        }
+
+        Kontostand += betrag;
+        Transaktion transaktion = new Transaktion(DateTime.Now, betrag, "Einzahlung");
+        transaktionen.Add(transaktion);
+    }
+
+    public void Auszahlen(decimal betrag)
+    {
+        if (betrag <= 0)
+        {
+            throw new ArgumentException("Der Auszahlungsbetrag muss größer als Null sein.");
+        }
+
+        if (betrag > Kontostand)
+        {
+            throw new ArgumentException("Der Auszahlungsbetrag ist größer als der Kontostand.");
+        }
+
+        Kontostand -= betrag;
+        Transaktion transaktion = new Transaktion(DateTime.Now, -betrag, "Auszahlung");
+        transaktionen.Add(transaktion);
+    }
+
+    public IReadOnlyList<Transaktion> Transaktionen
+    {
+        get { return transaktionen.AsReadOnly(); }
+    }
+}
---
+++ Uebungen/SW04/Buchhaltung/Buchhaltung/Bankkonto/Transaktion.cs
@@ -0,0 +1,15 @@
+﻿namespace Buchhaltung;
+
+public class Transaktion
+{
+    public DateTime Datum { get; set; }
+    public decimal Betrag { get; set; }
+    public string Beschreibung { get; set; }
+
+    public Transaktion(DateTime datum, decimal betrag, string beschreibung)
+    {
+        this.Datum = datum;
+        this.Betrag = betrag;
+        this.Beschreibung = beschreibung;
+    }
+}
