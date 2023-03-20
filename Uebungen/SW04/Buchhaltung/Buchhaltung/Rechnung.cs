a7f54d15dbf07826c40104f4b4b3aa9ed551bfa3
Simon Neidhart <simon.neidhart@hotmail.com>
2023-03-20 06:32:38 +0100

    add better encapsulated implementation

--- Uebungen/SW04/Buchhaltung/Buchhaltung/Rechnung.cs
+++ Uebungen/SW04/Buchhaltung/Buchhaltung/Rechnung.cs
@@ -4,5 +4,33 @@ namespace Buchhaltung;
 
 public class Rechnung
 {
-    public decimal BetragOffen { get; set; }
+    public Rechnung(decimal betragOffen)
+    {
+        BetragOffen = betragOffen;
+    }
+
+    public decimal BetragOffen { get; private set; }
+
+    public RechnungState State
+    {
+        get
+        {
+            if (BetragOffen > 0)
+            {
+                return RechnungState.Teilbezahlt;
+            }
+
+            if (BetragOffen == 0)
+            {
+                return RechnungState.Bezahlt;
+            }
+
+            return RechnungState.Ueberbezahlt;
+        }
+    }
+
+    public void Add(Zahlung zahlung)
+    {
+        BetragOffen -= zahlung.Betrag;
+    }
 }
