950304b390e21ea810350deb177223584621b698
Simon Neidhart <simon.neidhart@hotmail.com>
2023-05-07 21:44:45 +0200

    restructure projects

--- Beispiele im Unterricht/Constructors/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Constructors;
-
-Console.WriteLine("Hello, World!");
-Person person = new Person { Name = "Max Mustermann", Alter = 30 };
