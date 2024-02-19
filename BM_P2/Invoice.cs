af47a2c8485871738c0615e772b962751a14b4cb
Simon Neidhart <simon.neidhart@hotmail.com>
2024-02-19 16:21:37 +0100

    refactoring: move method to invoice

--- BM_P2/Invoice.cs
+++ BM_P2/Invoice.cs
@@ -11,4 +11,20 @@ public class Invoice
     // TODO: Properties sollten Readonly sein.
     public decimal Amount { get; }
     public decimal AmountOpen { get; set; }
+    
+    public InvoiceState AddPaymentToInvoice(Invoice invoice, Payment payment)
+    {
+        invoice.AmountOpen -= payment.Betrag;
+        if (invoice.AmountOpen > 0)
+        {
+            return InvoiceState.PartiallyPaid;
+        }
+
+        if (invoice.AmountOpen == 0)
+        {
+            return InvoiceState.Paid;
+        }
+
+        return InvoiceState.OverPaid;
+    }
 }
--- BM_P2/InvoiceService.cs
+++ BM_P2/InvoiceService.cs
@@ -2,20 +2,4 @@
 
 public class InvoiceService
 {
-    // TODO: verschieben dieser Methode in die Klasse Invoice, um die Kohäsion zu erhöhen.
-    public InvoiceState AddPaymentToInvoice(Invoice invoice, Payment payment)
-    {
-        invoice.AmountOpen -= payment.Betrag;
-        if (invoice.AmountOpen > 0)
-        {
-            return InvoiceState.PartiallyPaid;
-        }
-
-        if (invoice.AmountOpen == 0)
-        {
-            return InvoiceState.Paid;
-        }
-
-        return InvoiceState.OverPaid;
-    }
 }
