20dae4cf412661cb8c9e58aec0ad5a8eb7fc9610
Simon Neidhart <simon.neidhart@hotmail.com>
2024-04-08 16:58:42 +0200

    add solution

---
+++ BM_P2/StaticPolymorphism/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace BM_P2.StaticPolymorphism;
+
+public class Car
+{
+    private readonly int _accelerationKmhPerSecond;
+
+    public Car(int accelerationKmhPerSecond)
+    {
+        this._accelerationKmhPerSecond = accelerationKmhPerSecond;
+    }
+
+    public int Drive()
+    {
+        return _accelerationKmhPerSecond;
+    }
+}
