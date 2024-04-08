af52adba15b1fd9288be62b52615c2219303d59d
Simon Neidhart <simon.neidhart@hotmail.com>
2024-04-08 17:03:25 +0200

    add first test

---
+++ BM_P2.Tests/DynamicPolymorphism/AnimalTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace BM_P2.DynamicPolymorphism;
+
+public class AnimalTests
+{
+    [Fact]
+    public void AnimalsShouldMakeSounds()
+    {
+        var animalFarm = new AnimalFarm();
+        var animals = animalFarm.GetAllAnimals().ToList();
+        
+        animals.ForEach(animal =>
+        {
+            var animalSound = animal.MakeSound();
+            Console.WriteLine(animalSound);
+            
+            animalSound.Should().NotBeEmpty();
+        });
+    }
+}
---
+++ BM_P2/DynamicPolymorphism/AnimalFarm.cs
@@ -0,0 +1,10 @@
+﻿namespace BM_P2.DynamicPolymorphism;
+
+public class AnimalFarm
+{
+    public IEnumerable<IAnimal> GetAllAnimals()
+    {
+        var animals = new List<IAnimal>();
+        return animals;
+    }
+}
