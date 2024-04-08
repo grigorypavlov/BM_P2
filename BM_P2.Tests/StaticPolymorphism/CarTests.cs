59d87b4897043aa5e3bdc0a14ffcbc7d2f19c84b
Simon Neidhart <simon.neidhart@hotmail.com>
2024-04-08 16:58:18 +0200

    add static polymorphism test

---
+++ BM_P2.Tests/StaticPolymorphism/CarTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace BM_P2.StaticPolymorphism;
+
+public class CarTests
+{
+    private readonly Car _testee;
+
+    public CarTests()
+    {
+        _testee = new Car(3);
+    }
+
+    [Fact]
+    public void WhenCarDrivesForOneSecondItsSpeedShouldBeThreeKmh()
+    {
+        _testee.Drive().Should().Be(3);
+    }
+}
