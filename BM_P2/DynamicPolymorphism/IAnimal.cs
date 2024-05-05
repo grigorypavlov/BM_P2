1becb1d1a3527777da4a4905230bd538afbdffa5
Simon Neidhart <simon.neidhart@hotmail.com>
2024-05-05 17:17:54 +0200

    Beispiel aus Folien

--- BM_P2/DynamicPolymorphism/IAnimal.cs
+++ BM_P2/DynamicPolymorphism/IAnimal.cs
@@ -4,3 +4,31 @@ public interface IAnimal
 {
     string MakeSound();
 }
+public interface IDriveable
+{
+    void Forward();
+    void Backward();
+}
+
+class Car : IDriveable
+{
+    private readonly int _speedInKmh;
+
+    public Car(int speedInKmh)
+    {
+        _speedInKmh = speedInKmh;
+        RelativePosition = 0;
+    }
+    
+    public int RelativePosition { get; private set; }
+
+    public void Forward()
+    {
+        RelativePosition += _speedInKmh;
+    }
+
+    public void Backward()
+    {
+        RelativePosition -= _speedInKmh;
+    }
+}
